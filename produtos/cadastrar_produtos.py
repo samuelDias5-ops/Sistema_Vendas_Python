73ea3a662c9475a59ac1c58ade2808983c32235b
Samuel Dias <samuelsassenda5@gmail.com>
2026-09-14 23:18:17 +0100

    update cadastro

--- produtos/cadastrar_produtos.py
+++ produtos/cadastrar_produtos.py
@@ -1,37 +1,43 @@
-def pedir_numero(mensagem):
+def pedir_nome():
     while True:
         try:
-            mensagem 
-        except(TypeError,ValueError):
+            nome = input("Insira o produto que deseja cadastrar: ").strip().lower()
+        except(ValueError,TypeError, KeyboardInterrupt):
+            print("Insira um nome válido")
+            continue
+        else:
+            break
+
+def pedir_preco():
+
+      while True:
+        try:
+            preco = float(input("Insira o preco do produto: "))
+        except(ValueError, TypeError):
             print("Insira um valor válido!")
             continue
         else:
             break
 
-
-#def pedir_qtd():
-    #while True:
-        #try:
-         #   quantidade = int(input("Insira quantidade em estoque: "))
-        #except(ValueError, TypeError):
-          #  print("Insira um valor válido!")
-         #   continue
-        #else:
-            #break
+def pedir_qtd():
+    while True:
+        try:
+            quantidade = int(input("Insira quantidade em estoque: "))
+        except(ValueError, TypeError):
+            print("Insira um valor válido!")
+            continue
+        else:
+            break
     
 def cadastrar():
-    nome = input("Insira o produto que deseja cadastrar: ").strip().lower()
-    while True:
-        #valor = float(input("Insira preço de produto:"))
-        preco = pedir_numero("Insira preço do produto: ")
-        #quantidade = int(input("Insira quantidade em estoque: "))
-        qtd = pedir_numero("Insira quantidade em estoque: ")
-        produto = {
-                "nome": nome,
-                "preco": preco,
-                "qtd": qtd        
-        }
-        return produto
+    nome = pedir_nome()
+    preco = pedir_preco()
+    qtd = pedir_qtd()
+    produto = {
+            "nome": nome,
+            "preco": preco,
+            "qtd": qtd        
+    }
+    print(f"Nome: {nome}| Preço: {preco}Kz| Qtd: {qtd}, produto cadastrado com sucesso!")
+    return produto
 
-
-cadastrar()
